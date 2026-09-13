a84f46c4bfe2e98cac53bd19470f52f194d54634
jobbubbleapp <jobbubbleapp@gmail.com>
2026-09-13 16:50:11 -0700

    Run logo-disable prepatch before V9.4.66

--- scripts/apply_current.py
+++ scripts/apply_current.py
@@ -30,6 +30,7 @@ steps = [
     'v963_filter_control_clipping_fix.py',
     'v964_compact_top_controls.py',
     'v965_half_height_top_controls.py',
+    'v966a_disable_logo_request.py',
     'v966_legal_help_no_company_logos.py',
 ]
 
